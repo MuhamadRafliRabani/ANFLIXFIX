2ffdc8e31da41711434c720eb5decd6db5502e6b
MUHAMAD RAFLI RABANI <147303868+konoham@users.noreply.github.com>
2024-10-31 11:21:49 +0700

    Update filter.jsx

--- src/app/Components/ui/filter.jsx
+++ src/app/Components/ui/filter.jsx
@@ -42,7 +42,7 @@ const Filter = ({ handleOpen, isOpen }) => {
   console.log("🚀 ~ Filter ~ isOpen:", isOpen);
   return (
     <div
-      className={`fixed right-1/2 z-40 h-fit w-3/4 translate-x-1/2 rounded-lg border-neutral-400 p-4 transition-all duration-300 md:static md:right-0 md:h-3/4 md:w-[15%] md:translate-x-0 md:rounded-none md:border-e md:bg-transparent md:pt-0 ${isOpen ? "top-0" : "-top-full"}`}
+      className={`fixed right-1/2 z-40 h-fit bg-primary_color w-3/4 translate-x-1/2 rounded-lg border-neutral-400 p-4 transition-all duration-300 md:static md:right-0 md:h-3/4 md:w-[15%] md:translate-x-0 md:rounded-none md:border-e md:bg-transparent md:pt-0 ${isOpen ? "top-0" : "-top-full"}`}
     >
       <Accordion items={accordionItems} handleOpen={handleOpen} />
     </div>
