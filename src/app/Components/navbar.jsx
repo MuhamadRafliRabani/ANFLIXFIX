16534f7341cf1efac113aa387d938eccdc41d197
Noirrr <muhamadraflirabani@gmail.com>
2025-02-24 08:27:12 +0700

    layout: remove dark mode

--- src/app/Components/navbar.jsx
+++ src/app/Components/navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`fixed top-0 z-[9999] flex max-h-11 w-full translate-x-0 items-center justify-between bg-transparent px-2 py-3 text-white shadow-sm transition-all duration-300 md:justify-normal md:gap-8 md:px-8 dark:bg-white ${
+      className={`fixed top-0 z-[9999] flex max-h-11 w-full translate-x-0 items-center justify-between bg-transparent px-2 py-3 text-white shadow-sm transition-all duration-300 md:justify-normal md:gap-8 md:px-8 ${
         change ? "border-b border-white border-opacity-35" : "border-opacity-0"
       }`}
     >
